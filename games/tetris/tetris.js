89af55ea3f36287e289ce187ba04fa78382130bb
Likbess <likbes.nik@gmail.com>
2019-02-04 22:56:36 +0200

    add colors and fixed some bugs

--- games/tetris/tetris.js
+++ games/tetris/tetris.js
@@ -1,14 +1,28 @@
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
-
 context.scale(20, 20);
 
-const matrix = [
-  [0, 0, 0],
-  [1, 1, 1],
-  [0, 1, 0],
-];
+const player = {
+  pos: {
+    x: 5,
+    y: 5
+  },
+  matrix: createPiece('T')
+}
 
+const arena = createMatrix(12, 20);
+const colors = [
+  null,
+  '#FF0D72',
+  '#0DC2FF',
+  '#0DFF72',
+  '#F538FF',
+  '#FF8E0D',
+  '#FFE138',
+  '#3877FF'
+]
+
+// collissions
 function collide(arena, player) {
   const [m, o] = [player.matrix, player.pos];
   for (let y = 0; y < m.length; ++y) {
@@ -22,16 +36,6 @@ function collide(arena, player) {
   return false;
 }
 
-const player = {
-  pos: {
-    x: 5,
-    y: 5
-  },
-  matrix: matrix
-}
-
-const arena = createMatrix(12, 20);
-
 function createMatrix(w, h) {
   const matrix = [];
   while (h--) {
@@ -44,7 +48,7 @@ function drawMatricks(matrix, offset) {
   matrix.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value != 0) {
-        context.fillStyle = 'red';
+        context.fillStyle = colors[value];
         context.fillRect(
           x + offset.x,
           y + offset.y,
@@ -86,10 +90,31 @@ function playerDrop() {
   if (collide(arena, player)) {
     player.pos.y--;
     merge(arena, player);
-    player.pos.y = 0;
+    playerReset();
   }
   dropCounter = 0;
-};
+}
+
+function playerMove(dir) {
+  player.pos.x += dir;
+  if (collide(arena, player)) {
+    player.pos.x -= dir;
+  }
+}
+
+function playerReset() {
+  const pieces = 'ILJOTSZ';
+  player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
+  player.pos.y = 0;
+  player.pos.x =
+    (arena[0].length / 2 | 0) -
+    (player.matrix[0].length / 2 | 0);
+
+  if (collide(arena, player)) {
+    arena.forEach(row => row.fill(0));
+  }
+
+}
 
 function update(time = 0) {
   const deltaTime = time - lastTime;
@@ -102,22 +127,130 @@ function update(time = 0) {
   requestAnimationFrame(update);
 };
 
+// rotate
 
+function playerRotate(dir) {
+  const pos = player.pos.x;
+  let offset = 1;
+  rotate(player.matrix, dir);
+  while (collide(arena, player)) {
+    player.pos.x += offset;
+    offset = -(offset + (offset > 0 ? 1 : -1))
+    if (offset > player.matrix[0].length) {
+      rotate(player.matrix, -dir);
+      player.pos.x = pos;
+      return;
+    }
+  }
+}
+
+function rotate(matrix, dir) {
+  for (let y = 0; y < matrix.length; ++y) {
+    for (let x = 0; x < y; ++x) {
+      [
+        matrix[x][y],
+        matrix[y][x],
+      ] = [
+        matrix[y][x],
+        matrix[x][y]
+      ]
+    }
+  }
+
+  if (dir > 0) {
+    matrix.forEach(row => row.reverse());
+  } else {
+    matrix.reverse();
+  }
+}
+
+// pieces
+
+function createPiece(type) {
+  if (type == 'T') {
+    return [
+      [0, 0, 0],
+      [1, 1, 1],
+      [0, 1, 0]
+    ];
+  }
+
+  if (type == 'J') {
+    return [
+      [0, 2, 0],
+      [0, 2, 0],
+      [2, 2, 0]
+    ];
+  }
+
+  if (type == 'L') {
+    return [
+      [0, 3, 0],
+      [0, 3, 0],
+      [0, 3, 3]
+    ];
+  }
+
+  if (type == 'O') {
+    return [
+      [4, 4],
+      [4, 4]
+    ];
+  }
+
+  if (type == 'I') {
+    return [
+      [0, 5, 0, 0],
+      [0, 5, 0, 0],
+      [0, 5, 0, 0],
+      [0, 5, 0, 0]
+    ];
+  }
+
+  if (type == 'S') {
+    return [
+      [0, 6, 6],
+      [6, 6, 0],
+      [0, 0, 0]
+    ];
+  }
+
+  if (type == 'Z') {
+    return [
+      [7, 7, 0],
+      [0, 7, 7],
+      [0, 0, 0]
+    ];
+  }
+}
 
 // Events
 
 document.addEventListener('keydown', e => {
 
+  // left
   if (e.keyCode == 37) {
-    player.pos.x--;
+    playerMove(-1);
   }
 
+  // right
   if (e.keyCode == 39) {
-    player.pos.x++;
+    playerMove(1);
   }
 
+  // down
   if (e.keyCode == 40) {
     playerDrop();
+  }
+
+  // q
+  if (e.keyCode == 81) {
+    playerRotate(-1);
+  }
+
+  // w
+  if (e.keyCode == 87) {
+    playerRotate(1);
   }
 
 });
