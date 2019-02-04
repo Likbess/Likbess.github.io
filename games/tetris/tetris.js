a7ce7543981120876238e91dd6b1fa8b8a4d0e34
Likbess <likbes.nik@gmail.com>
2019-02-04 20:48:39 +0200

    add oportunity to have more than one block

--- games/tetris/tetris.js
+++ games/tetris/tetris.js
@@ -9,10 +9,35 @@ const matrix = [
   [0, 1, 0],
 ];
 
-function draw() {
-  context.fillStyle = '#000';
-  context.fillRect(0, 0, canvas.width, canvas.height);
-  drawMatricks(player.matrix, player.pos);
+function collide(arena, player) {
+  const [m, o] = [player.matrix, player.pos];
+  for (let y = 0; y < m.length; ++y) {
+    for (let x = 0; x < m[y].length; ++x) {
+      if (m[y][x] != 0 &&
+        (arena[y + o.y] &&
+          arena[y + o.y][x + o.x]) != 0)
+        return true;
+    }
+  }
+  return false;
+}
+
+const player = {
+  pos: {
+    x: 5,
+    y: 5
+  },
+  matrix: matrix
+}
+
+const arena = createMatrix(12, 20);
+
+function createMatrix(w, h) {
+  const matrix = [];
+  while (h--) {
+    matrix.push(new Array(w).fill(0));
+  }
+  return matrix;
 }
 
 function drawMatricks(matrix, offset) {
@@ -24,36 +49,77 @@ function drawMatricks(matrix, offset) {
           x + offset.x,
           y + offset.y,
           1, 1);
-      };
+      }
     });
   });
 };
 
-const player = {
-  pos: {
-    x: 5,
-    y: 5
-  },
-  matrix: matrix
-}
+function draw() {
+  context.fillStyle = '#000';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  drawMatricks(arena, {
+    x: 0,
+    y: 0,
+  })
+  drawMatricks(player.matrix, player.pos);
+};
+
+function merge(arena, player) {
+  player.matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value != 0) {
+        arena[y + player.pos.y][x + player.pos.x] = value;
+      }
+    });
+  });
+};
+
+// draw movement tetrix every second
 
 let lastTime = 0;
 
 let dropCounter = 0;
 let dropInterval = 1000;
 
+function playerDrop() {
+  player.pos.y++;
+  if (collide(arena, player)) {
+    player.pos.y--;
+    merge(arena, player);
+    player.pos.y = 0;
+  }
+  dropCounter = 0;
+};
+
 function update(time = 0) {
   const deltaTime = time - lastTime;
   lastTime = time;
 
   dropCounter += deltaTime;
-  if (dropCounter > dropInterval) {
-    player.pos.y++;
-    dropCounter = 0;
-  }
+  if (dropCounter > dropInterval) playerDrop();
 
   draw();
   requestAnimationFrame(update);
-}
+};
+
+
+
+// Events
+
+document.addEventListener('keydown', e => {
+
+  if (e.keyCode == 37) {
+    player.pos.x--;
+  }
+
+  if (e.keyCode == 39) {
+    player.pos.x++;
+  }
+
+  if (e.keyCode == 40) {
+    playerDrop();
+  }
+
+});
 
 update();
